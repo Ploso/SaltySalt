ce854ceba96f3459a0fc082d2ce7e04d2c364d64
Ploso <eero.leppaniemi@gmail.com>
2015-06-30 11:30:18 +0300

    Alkucommit

---
+++ Assets/Scripts/GameGlobals.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGlobals : MonoBehaviour {
+
+	public static int salt;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+	public static int GetSalt ()
+	{
+		return salt;
+	}
+
+	public static void SetSalt (int tempSalt)
+	{
+		salt = tempSalt;
+	}
+}
