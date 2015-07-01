5af2e20af3e42ec763d1dfafe18a663760d66186
Ploso <eero.leppaniemi@gmail.com>
2015-07-01 15:46:10 +0300

    commitz

--- Assets/Scripts/GameGlobals.cs
+++ Assets/Scripts/GameGlobals.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class GameGlobals : MonoBehaviour {
 
-	public static int salt;
+	public static float salt;
 	public static float rage;
 	public static float satisfaction;
 	public static int seaWater;
+	public static int winCount;
+	public static bool keyboard = true;
+	public static int keyboardHealth = 5;
+
+	public static bool rageQuit;
+	public static bool noMoney;
+	public static bool salted;
 
 	void Start () {
 	
@@ -17,12 +24,12 @@ public class GameGlobals : MonoBehaviour {
 	
 	}
 
-	public static int GetSalt ()
+	public static float GetSalt ()
 	{
 		return salt;
 	}
 
-	public static void SetSalt (int tempSalt)
+	public static void SetSalt (float tempSalt)
 	{
 		salt = tempSalt;
 	}	
@@ -44,4 +51,66 @@ public class GameGlobals : MonoBehaviour {
 	{
 		satisfaction = tempSatisfaction;
 	}
+
+	public static int GetWins ()
+	{
+		return winCount;
+	}
+	
+	public static void SetWins (int tempWin)
+	{
+		winCount = tempWin;
+	}
+
+	public static bool GetKeyboard ()
+	{
+		return keyboard;
+	}
+	
+	public static void SetKeyboard (bool tempKey)
+	{
+		keyboard = tempKey;
+	}
+
+	public static int GetKeyboardHealth ()
+	{
+		return keyboardHealth;
+	}
+	
+	public static void SetKeyBoardHealth (int tempKBH)
+	{
+		keyboardHealth = tempKBH;
+	}
+
+	public static bool GetRageQuit()
+	{
+		return rageQuit;
+	}
+	
+	public static void SetRageQuit (bool tempRQ)
+	{
+		rageQuit = tempRQ;
+	}
+
+	public static bool GetNoMoney ()
+	{
+		return noMoney;
+	}
+	
+	public static void SetNoMoney (bool tempNM)
+	{
+		noMoney = tempNM;
+	}
+
+	public static bool GetSalted ()
+	{
+		return salted;
+	}
+	
+	public static void SetSalted (bool tempSalted)
+	{
+		salted = tempSalted;
+	}
+
+
 }
--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -6,6 +6,8 @@ public class GameScript : MonoBehaviour {
 
 	public int count;
 	public int levelLength;
+	public int success;
+	public int victories;
 
 	public float timer;
 	public float delay;
@@ -17,31 +19,49 @@ public class GameScript : MonoBehaviour {
 
 	private float rage;
 	private float satisfaction;
-	private int salt;
+	private float salt;
 
 	public Button left;
 	public Button middle;
 	public Button right;
 	public Button startButton;
+	public Button cookScene;
+	public Button buyNewKeyboard;
 
 	public Text rageText;
 	public Text satisfactionText;
+	public Text winAmount;
 
 	private string screenRage;
 	private string screenSatisfaction;
+	private string screenVictories;
 
 	private bool levelPlaying;
 
+	public Slider rageMeter;
+	public Slider satisfactionMeter;
+	public Slider saltMeter;
+
+	public bool keyboard;
+	public int keyboardHealth;
+	public GameObject broken;
 
 	void Start () {
+
+		victories = GameGlobals.GetWins ();
 		salt = GameGlobals.GetSalt ();
 		rage = GameGlobals.GetRage ();
 		satisfaction = GameGlobals.GetSatisfaction ();
 
+		rageMeter = rageMeter.GetComponent<Slider> ();
+		satisfactionMeter = satisfactionMeter.GetComponent<Slider> ();
+		saltMeter = saltMeter.GetComponent<Slider> ();
 		left = left.GetComponent<Button> ();
 		middle = middle.GetComponent<Button> ();
 		right = right.GetComponent<Button> ();
 		startButton = startButton.GetComponent<Button> ();
+		cookScene = cookScene.GetComponent<Button> ();
+		buyNewKeyboard = buyNewKeyboard.GetComponent<Button> ();
 
 		left.interactable = false;
 		middle.interactable = false;
@@ -55,7 +75,13 @@ public class GameScript : MonoBehaviour {
 		reactionTime = 50f;
 		levelLength = 8;
 
+		rageMeter.value = rage;
+		satisfactionMeter.value = satisfaction;
+		saltMeter.value = salt;
+
 		levelPlaying = false;
+		keyboard = GameGlobals.GetKeyboard ();
+		keyboardHealth = GameGlobals.GetKeyboardHealth ();
 
 	}
 	
@@ -66,66 +92,106 @@ public class GameScript : MonoBehaviour {
 
 		screenRage = rage.ToString();
 		screenSatisfaction = satisfaction.ToString();
+		screenVictories = victories.ToString ();
 
 		rageText.text = ("Rage: " + screenRage);
 		satisfactionText.text = ("Satisfaction: " + screenSatisfaction);
+		winAmount.text = ("Victories: " + screenVictories);
 
-		if (levelPlaying == true) {
+		if (levelPlaying == true && keyboard == true) {
 			if (Input.GetButtonDown ("Fire1")) {
 				if (qLeft == true) {
 					satisfaction = satisfaction + 0.5f;
+					success++;
 					QSwitchFalse ();
 					screenSatisfaction = satisfaction.ToString ();
-					satisfactionText.text = ("Satisfaction: " + screenSatisfaction);
+					satisfactionText.text = ("Satisfaction: " + screenSatisfaction);		
+					satisfactionMeter.value = satisfaction;
 				} else { 
 					rage = rage + 1f;
 					rageText.text = ("Rage: " + screenRage);
 					screenRage = rage.ToString ();
+					rageMeter.value = rage;
 				}
 			}
 		}
 
-		if (levelPlaying == true) {
+		if (levelPlaying == true && keyboard == true) {
 			if (Input.GetButtonDown ("Fire2")) {
 				if (wMiddle == true) {
 					satisfaction = satisfaction + 0.5f;
+					success++;
 					WSwitchFalse ();
 					screenSatisfaction = satisfaction.ToString ();
 					satisfactionText.text = ("Satisfaction: " + screenSatisfaction);
+					satisfactionMeter.value = satisfaction;
 				} else {
 					rage = rage + 1f;
 					rageText.text = ("Rage: " + screenRage);
 					screenRage = rage.ToString ();
+					rageMeter.value = rage;
 				}
 			}
 		}
 
-		if (levelPlaying == true) {
+		if (levelPlaying == true && keyboard == true) {
 			if (Input.GetButtonDown ("Fire3")) {
 				if (eRight == true) {
 					satisfaction = satisfaction + 0.5f;
+					success++;
 					ESwitchFalse ();
 					screenSatisfaction = satisfaction.ToString ();
 					satisfactionText.text = ("Satisfaction: " + screenSatisfaction);
+					satisfactionMeter.value = satisfaction;
 				} else {
 					rage = rage + 1f;
 					rageText.text = ("Rage: " + screenRage);
 					screenRage = rage.ToString ();
+					rageMeter.value = rage;
 				}
+			}
+		}
+
+		if (Input.GetButtonDown("Jump") && keyboard == true){
+			keyboardHealth--;
+			rage = rage - 10;
+			GameGlobals.SetRage(rage);
+			if (keyboardHealth <= 0){
+				Instantiate (broken, new Vector2 (0, 0), Quaternion.identity);
+				keyboard = false;
+				GameGlobals.SetKeyboard(keyboard);
 			}
 		}
 
 		if (timer >= reactionTime && qLeft == true) {
 			rage = rage + 1f;
 			QSwitchFalse();
+			rageMeter.value = rage;
 		}
 		if (timer >= reactionTime && wMiddle == true) {
 			rage = rage + 1f;
 			WSwitchFalse();
+			rageMeter.value = rage;
 		}
 		if (timer >= reactionTime && eRight == true) {
 			rage = rage + 1f;
 			ESwitchFalse();
+			rageMeter.value = rage;
+		}
+
+		if (keyboard == false) {
+			buyNewKeyboard.interactable = true;
+		} else {
+			buyNewKeyboard.interactable = false;
+		}
+
+		if (rage < 0) {
+			rage = 0;
+			GameGlobals.SetRage(rage);
+		}
+		if (satisfaction < 0) {
+			satisfaction = 0;
+			GameGlobals.SetSatisfaction(satisfaction);
 		}
 	}
 
@@ -199,8 +265,21 @@ public class GameScript : MonoBehaviour {
 				Invoke ("WSwitchTrue", delay);
 			}
 		} else {
+			if (success > levelLength / 2){
+				victories++;
+			} else{
+				rage = rage + 10;
+			}
+			if (keyboard == true){
+				keyboardHealth = 5;
+				GameGlobals.SetKeyBoardHealth(keyboardHealth);
+			}
+			GameGlobals.SetWins(victories);
+			GameGlobals.SetSatisfaction (satisfaction);
+			GameGlobals.SetRage (rage);
 			startButton.interactable = true;
 			levelPlaying = false;
+			success = 0;
 		}
 	}
 
@@ -209,6 +288,7 @@ public class GameScript : MonoBehaviour {
 		levelPlaying = true;
 		count = 0;
 		timer = 0;
+		success = 0;
 		GameStarter ();
 		left.interactable = false;
 		middle.interactable = false;
@@ -225,6 +305,7 @@ public class GameScript : MonoBehaviour {
 		levelPlaying = true;
 		count = 0;
 		timer = 0;
+		success = 0;
 		GameStarter ();
 		left.interactable = false;
 		middle.interactable = false;
@@ -239,4 +320,24 @@ public class GameScript : MonoBehaviour {
 		eRight = false;
 		startButton.interactable = false;
 	}
+
+	public void ToTheKitchen()
+	{
+		Application.LoadLevel (2);
+	}
+
+	public void BuyNewKeyboard()
+	{
+		if (satisfaction > 1) {
+			satisfaction = satisfaction - 5;
+			rage = rage + 10;
+			salt = salt - 100;
+			GameGlobals.SetSatisfaction (satisfaction);
+			GameGlobals.SetRage (rage);
+			GameGlobals.SetSalt (salt);
+		} else {
+			Application.LoadLevel(3);
+		}
+	}
+	
 }
